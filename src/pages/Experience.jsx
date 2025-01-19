a250c93d0f63867930705b0ae5d76db0013125fd
PallaviSax <pallavi.saxena0731@gmail.com>
2025-01-19 18:06:58 -0500

    Update Experience.jsx

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -14,15 +14,15 @@ function Experience() {
           <strong>Work Experience</strong>
           </p>
             <p>
-            Research Intern, Child’s Environmental Health & Disease Research Center (June 2023 - Aug 2023): As a Research Intern, I conducted <a href="https://docs.google.com/document/d/1UBdsiESGJ-H2q6EhcO55LpyINK6Log5HOL3JS4pqTvA/edit?usp=sharing">literature reviews</a> using PubMed to examine the associations between maternal autoimmune conditions and infections during pregnancy and infancy with subsequent psychiatric disorders. My work informed ongoing research led by Dartmouth faculty, including Dr. Hannah Laue. Additionally, I completed an epidemiology course to deepen my understanding of disease frequency, study designs, and causality, equipping me with foundational knowledge for public health research.
-            </p>
-            <p>
             Research Intern, Center for the Study of Sport in Society, Northeastern University (May 2022 - Aug 2022):
             During my time at the Center for the Study of Sport in Society, I researched secondary literature on hate speech, reentry programs for previously incarcerated individuals, and initiatives empowering urban youth and people of color in the Boston area. I wrote literature reviews under tight deadlines on current events, contributing to the executive director’s strategic decisions for community-focused programs like the Toxic Speech Prevention Program.
             </p>
             <p>
             Social Media Marketing Intern, Platform Wellness (June 2023 - July 2023):
             As a Social Media Marketing Intern, I researched, wrote, and published four 800-word <a href="https://www.shellybhowmik.com/blog/modeling-healthy-habits-for-your-team">articles</a>     on burnout prevention for women of color in the workplace. I also developed a year-long content calendar featuring infographics and videos tailored to predicted trends, which were shared on LinkedIn, YouTube, and Instagram to enhance the business’s online presence.
+            </p>
+            <p>
+            Research Intern, Child’s Environmental Health & Disease Research Center (June 2023 - Aug 2023): As a Research Intern, I conducted <a href="https://docs.google.com/document/d/1UBdsiESGJ-H2q6EhcO55LpyINK6Log5HOL3JS4pqTvA/edit?usp=sharing">literature reviews</a> using PubMed to examine the associations between maternal autoimmune conditions and infections during pregnancy and infancy with subsequent psychiatric disorders. My work informed ongoing research led by Dartmouth faculty, including Dr. Hannah Laue. Additionally, I completed an epidemiology course to deepen my understanding of disease frequency, study designs, and causality, equipping me with foundational knowledge for public health research.
             </p>
             <p>
             Marketing Intern, Montclair ADHD Coaching (Dec 2024 - Jan 2025):
